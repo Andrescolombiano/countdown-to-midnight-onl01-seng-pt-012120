95c79cd330dc2df9210d28759045fd3604ee4ece
Andres Carvajal <jc1993box@hotmail.com>
2020-01-27 18:54:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
-
-def countdown
+def countdown (number)
+ while number > 0 
+ number -= 1
+  puts "#{number}SECOND(S)!"
+ end
+  "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(number)
+while number > 0
+  puts "#{number} SECOND(S)!"
+  sleep(1.0)
+  number -= 1
+end
+"HAPPY NEW YEAR!"
+end	
